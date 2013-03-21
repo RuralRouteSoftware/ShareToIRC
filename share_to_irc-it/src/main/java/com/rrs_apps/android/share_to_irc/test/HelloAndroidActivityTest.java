bac4812f163ef8e587fc10b2ba780668ecaf0859
Cameron Currie <me@cameroncurrie.net>
2013-03-21 18:19:05 -0500

    Add AndroidAnnotations

--- share_to_irc-it/src/main/java/com/rrs_apps/android/share_to_irc/test/HelloAndroidActivityTest.java
+++ share_to_irc-it/src/main/java/com/rrs_apps/android/share_to_irc/test/HelloAndroidActivityTest.java
@@ -3,11 +3,12 @@ package com.rrs_apps.android.share_to_irc.test;
 import android.test.ActivityInstrumentationTestCase2;
 
 import com.rrs_apps.android.share_to_irc.HelloAndroidActivity;
+import com.rrs_apps.android.share_to_irc.HelloAndroidActivity_;
 
-public class HelloAndroidActivityTest extends ActivityInstrumentationTestCase2<HelloAndroidActivity> {
+public class HelloAndroidActivityTest extends ActivityInstrumentationTestCase2<HelloAndroidActivity_> {
 
     public HelloAndroidActivityTest() {
-        super(HelloAndroidActivity.class);
+        super(HelloAndroidActivity_.class);
     }
 
     public void testActivity() {
--- share_to_irc/src/main/java/com/rrs_apps/android/share_to_irc/HelloAndroidActivity.java
+++ share_to_irc/src/main/java/com/rrs_apps/android/share_to_irc/HelloAndroidActivity.java
@@ -1,24 +1,8 @@
 package com.rrs_apps.android.share_to_irc;
 
-import android.os.Bundle;
-import android.util.Log;
-
 import com.actionbarsherlock.app.SherlockFragmentActivity;
+import com.googlecode.androidannotations.annotations.EActivity;
 
+@EActivity(R.layout.main)
 public class HelloAndroidActivity extends SherlockFragmentActivity {
-    private static String TAG = "share_to_irc";
-
-    /**
-     * Called when the activity is first created.
-     * 
-     * @param savedInstanceState
-     *            If the activity is being re-initialized after previously being shut down then this Bundle contains the
-     *            data it most recently supplied in onSaveInstanceState(Bundle). <b>Note: Otherwise it is null.</b>
-     */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        Log.i(TAG, "onCreate");
-        setContentView(R.layout.main);
-    }
 }
