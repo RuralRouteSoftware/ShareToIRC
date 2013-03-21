d60158a578ec1ca4100ae6ef58c22911cc54f78e
Cameron Currie <me@cameroncurrie.net>
2013-03-21 13:39:17 -0500

    Add empty project

---
+++ share_to_irc-it/src/main/java/com/rrs_apps/android/share_to_irc/test/HelloAndroidActivityTest.java
@@ -0,0 +1,17 @@
+package com.rrs_apps.android.share_to_irc.test;
+
+import android.test.ActivityInstrumentationTestCase2;
+import com.rrs_apps.android.share_to_irc.*;
+
+public class HelloAndroidActivityTest extends ActivityInstrumentationTestCase2<HelloAndroidActivity> {
+
+    public HelloAndroidActivityTest() {
+        super(HelloAndroidActivity.class); 
+    }
+
+    public void testActivity() {
+        HelloAndroidActivity activity = getActivity();
+        assertNotNull(activity);
+    }
+}
+
